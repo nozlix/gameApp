395dcacfab37e01d62940baa5207f5ecdb3a69ec
ravelonarivo <finaritraravelonarivo@gmail.com>
2025-03-21 11:07:16 +0100

    add mazeGenerator for random labyrinth

--- app/src/main/java/com/example/gameapp/GameView.java
+++ app/src/main/java/com/example/gameapp/GameView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,6 +21,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private int startX;
     private int startY;
 
+
     private Context context;
 
     public GameView(Context context, int valeur_y) {
@@ -29,6 +31,12 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         thread = new GameThread(getHolder(), this);
         setFocusable(true);
         this.y = valeur_y;
+
+        MazeGenerator generator = new MazeGenerator(10, 10);
+        labyrinth = generator.getMaze();
+
+
+
     }
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width,
@@ -64,50 +72,35 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             canvas.drawRect(x, y, x + 100, y + 100, paint);
         }
 
-        initializeLabyrinth();
         drawLabyrinth(canvas);
     }
     public void update() {
         x = (x + 1) % 300;
     }
 
-    // Function to initialize the labyrinth (example)
-    private void initializeLabyrinth() {
-        // Example: a simple 15x15 labyrinth
-        labyrinth = new int[][]{
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1},
-                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-                {1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-                {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1},
-                {1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1},
-                {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},
-                {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
-                {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-        };
-    }
-
     // Function to draw the labyrinth
     public void drawLabyrinth(Canvas canvas) {
-        this.initializeLabyrinth();
+        //this.initializeLabyrinth();
         this.calculateLabyrinthDimensions(canvas.getWidth(), canvas.getHeight());
-        if (labyrinth == null) return;
-
+        if (labyrinth == null){
+            return;
+        }
         Paint wallPaint = new Paint();
         wallPaint.setColor(Color.BLACK);
+
+        Paint pathPaint = new Paint();
+        pathPaint.setColor(Color.GREEN);  // Blanc pour les chemins
+        pathPaint.setStyle(Paint.Style.FILL);
 
         for (int i = 0; i < labyrinth.length; i++) {
             for (int j = 0; j < labyrinth[i].length; j++) {
                 if (labyrinth[i][j] == 1) { // 1 represents a wall
                     canvas.drawRect(startX + j * cellSize, startY + i * cellSize,
                             startX + (j + 1) * cellSize, startY + (i + 1) * cellSize, wallPaint);
-                }
+                } /*else {  // Chemin
+                    canvas.drawRect(startX + j * cellSize, startY + i  * cellSize,
+                            startX + (j + 1) * cellSize, startY + (i + 1) * cellSize, pathPaint);
+                }*/
             }
         }
     }
@@ -125,7 +118,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         int maxCellSize = Math.min(screenWidth / labyrinth[0].length, screenHeight / labyrinth.length);
 
         // Choose a cell size that is a bit smaller than the maximum to add some padding
-        cellSize = (int) (maxCellSize * 0.8); // 80% of the maximum size
+        cellSize = (int) (maxCellSize * 1); // 80% of the maximum size
 
         // Calculate the total width and height of the labyrinth
         int labyrinthWidth = labyrinth[0].length * cellSize;
@@ -135,5 +128,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         startX = (screenWidth - labyrinthWidth) / 2;
         startY = (screenHeight - labyrinthHeight) / 2;
     }
+
+
+
 
 }
